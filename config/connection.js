f61d62d77beb3de2df8496be23f67a4cd81b6780
origami unicorn <rvwright@gmail.com>
2020-01-25 10:07:38 -0800

    Added JAWSDB environmental variable.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
 // Set up connection to Pie DB
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "h3ll0",
-    database: "pie_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "h3ll0",
+        database: "pie_db"
+    })
+}
 
 // Set up connection
 connection.connect(function (err) {
