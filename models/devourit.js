2614244647d98f192f069cbc7ab7671a66ac5be2
origami unicorn <rvwright@gmail.com>
2020-01-25 16:14:38 -0800

    Cleaned up code, added comments for clarity.

--- models/devourit.js
+++ models/devourit.js
@@ -1,23 +1,27 @@
 // import orm from its folder to here
 var orm = require("../config/orm.js");
 
+// make the pie variable and define the object functions
 var pie = {
+    // selectAll to take all pies from the db
     selectAll: function (cb) {
         orm.selectAll("pies", function (res) {
             cb(res);
         });
     },
-    // The variables cols and vals are arrays.
+    // insertOne to add one row of data to pie table in db
     insertOne: function (cols, vals, cb) {
         orm.insertOne("pies", cols, vals, function (res) {
             cb(res);
         });
     },
+    // updateOne to update devoured boolean in pie table in db
     updateOne: function (objColVals, condition, cb) {
         orm.updateOne("pies", objColVals, condition, function (res) {
             cb(res);
         });
     }
-}
+};
 
+// exort pie to where it's required
 module.exports = pie;
